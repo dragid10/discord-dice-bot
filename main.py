9c22645fa8974acc5693a275e26b65d2ce767c63
Alex Oladele <dragid10@gmail.com>
2024-06-17 04:10:40 +0000

    ✨ Create an `on_ready` event listener and add step documentation
    The `on_ready` listener is the early phase of the discord bot lifecycle

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
+import os
 import discord
 
+from dice_bot_pyohio import config
 
-discord_bot = discord.Bot()
+# 1. Instantiate Discord bot object
+discord_bot: discord.Bot = discord.Bot()
+
+# 3. Discord bot first goes to the `on_ready` method
+@discord_bot.event # Decorator that waits for events to happen in Discord
+async def on_ready():
+    print("Starting up the bot!")
+
 
 if __name__ == "__main__":
-    # Set up Bot
-    pass
+    # 2. Pass Discord api token to bot (wrap in try/catch block to be safe)
+    try:
+        # 2.1. The Bot run command will actually start up the discord bot lifecycle
+        discord_bot.run(os.getenv(f"{config.DISCORD_TOKEN}"))
+    except RuntimeError as run_err:
+        print(run_err)
