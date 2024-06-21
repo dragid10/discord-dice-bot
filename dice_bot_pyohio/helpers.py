eaf4c47cd20d9e2235cd1b2045d035d2b7a0492b
Alex Oladele <dragid10@gmail.com>
2024-06-21 03:44:52 +0000

    ⚡️ Actually roll dice!
    We did it! It worksss

--- dice_bot_pyohio/helpers.py
+++ dice_bot_pyohio/helpers.py
@@ -26,9 +26,7 @@ def roll_die(dice_quantity: int, die_face: int) -> int:
     return roll_total
 
 def parse_dice_roll(roll: str) -> list[int]:
-        # Add a 1 to the front of the roll if the roll starts with a 'd'
-    if roll[0] == "d":
-        roll = f"1{roll}"
+    roll_result: list[int] = []
 
     # Split user text into individual parts: 1d20 -> ["1", "20"]
     # Example invocation: 2d6, "3d6+2d8", "3d20+6"
@@ -37,19 +35,28 @@ def parse_dice_roll(roll: str) -> list[int]:
     roll_parts: list[str] = roll.split("+")
 
     # Go through each of the individual roll parts and parse them
-    for part in roll_parts:
+    for die in roll_parts:
+        # Add a 1 to the front of the roll if the roll starts with a 'd'
+        if die[0] == "d":
+            die = f"1{die}"
+
         # Split the part into individual parts: 1d20 -> ["1", "20"]
         # Example invocation: 2d6, 3d6, 3d20
         # [3, 6]
         # [3, 20]
-        dice_roll: list[str] = part.split("d")
+        dice_roll: list[str] = die.split("d")
+
 
         # If the parsed component is not 2 (number of dice and die face), then the roll is invalid
         if len(dice_roll) != 2:
             return None
-        
+
         # Activate the random Number Generator!
-        roll_result = roll_die(int(dice_roll[0]), int(dice_roll[1])
+        result: int = roll_die(int(dice_roll[0]), int(dice_roll[1]))
+        print(f"Roll result: {result}")
+        roll_result.append(result)
+
+    return roll_result
 
 def is_valid_roll(user_input: str) -> bool:
     """Determine if the user's input is a valid roll. A valid roll should contain the letter 'd' and be like "1d12"
